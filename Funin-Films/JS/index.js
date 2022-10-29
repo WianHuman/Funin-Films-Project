e51032e547c7f663e1af6c175f0438c8d9c528c2
WianHuman <221286@virtualwindow.co.za>
2022-10-29 18:57:45 +0200

    Added Functionality to signin
    Added functionality to Sign In function on index page and saving the current username to Session storage
    
    IF YOU WANT TO DISPLAY THE CURRENT USERNAME ON YOUR PAGE USE saveduser.username IN YOUR JS

--- Funin-Films/JS/index.js
+++ Funin-Films/JS/index.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function (){
 
+    $("#errortext1").hide()
     $(".Signupblock").hide()
     $(".Signinblock").show()
 
@@ -29,7 +30,53 @@ $(document).ready(function (){
 
 })
 
+var UsEr = [
+    {
+        username: "Test1",
+        password: "12345"
+    },
+    {
+        username: "Test2",
+        password: "12345"
+    }
+]
 
+function login () {
+
+    var name = document.getElementById("user-name1").value;
+    var password = document.getElementById("pass-word1").value;
+    var usercheck = false
+
+
+    //UsEr is an array for testing, to be replaced with an actual array
+
+    for (let i = 0; i < UsEr.length; i++) {
+        if (name == UsEr[i].username) {
+            if (password == UsEr[i].password) {
+                usercheck = true
+            }
+        }
+    }
+
+    if (usercheck) {
+
+        var cuser = {
+            username: name,
+            password: password
+        }
+
+        var currentuser = JSON.stringify(cuser)
+        sessionStorage.setItem("loggedin",currentuser)
+        var saveduser = JSON.parse(sessionStorage.getItem("loggedin"))
+        location.replace("Home page.html")
+        // alert(saveduser.username)
+        // !!!use saveduser.username to display current user's username on your page using js or jquery!!!
+    }else {
+        $("#errortext1").show()
+        $("#errortext1").html("Incorrect Username or Password")
+    }
+
+}
 
 
 
